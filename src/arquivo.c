1791c61abdc392ba043d743b031407ecd5e57b33
Nyeidson Ryan <58894418+Tnbryan@users.noreply.github.com>
2026-06-01 23:47:17 +0000

    Prototipos dos Menus

---
+++ src/arquivo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// os printf é apenas para o compilador executar o codigo, porque ele vazio não funciona
+// quando for fazer , pode apagar e alterar
+
+void salvarProdutos() {
+    printf("Salvar produtos\n");
+}
+void carregarProdutos() {
+    printf("Carregar produtos\n");
+}
+void salvarPedidos() {
+    printf("Salvar pedidos\n");
+}
+void carregarPedidos() {
+    printf("Carregar pedidos\n");
+}
---
+++ src/pedido.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// os printf é apenas para o compilador executar o codigo, porque ele vazio não funciona
+// quando for fazer , pode apagar e alterar
+
+
+
+
+void criarPedido() {
+    printf("Criar pedido\n");
+}
+void listarPedidos() {
+    printf("Listar pedidos\n");
+}
