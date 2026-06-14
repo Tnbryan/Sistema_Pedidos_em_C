dc3100a1eba68e188beeb9e156a3dfea5e1f9abb
CarlosEduardo0702 <166566241+CarlosEduardo0702@users.noreply.github.com>
2026-06-14 18:23:08 -0300

    Implement product management functions
    Added product management functions including registration, listing, updating, and deletion.

--- src/produto.c
+++ src/produto.c
@@ -16,3 +16,174 @@ void alterarProduto() {
 void excluirProduto() {
     printf("Excluir produto\n");
 }
+
+//Código teste  Caso dê algum problema pode falar comigo. 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void cadastrarProduto() {
+
+    FILE *arquivo;
+    int id;
+    char nome[50];
+    float preco;
+
+    arquivo = fopen("data/produtos.txt", "a");
+
+    if (arquivo == NULL) {
+        printf("Erro ao abrir arquivo!\n");
+        return;
+    }
+
+    printf("\n=== CADASTRAR PRODUTO ===\n");
+
+    printf("ID: ");
+    scanf("%d", &id);
+
+    printf("Nome: ");
+    scanf(" %[^\n]", nome);
+
+    printf("Preco: ");
+    scanf("%f", &preco);
+
+    fprintf(arquivo, "%d;%s;%.2f\n", id, nome, preco);
+
+    fclose(arquivo);
+
+    printf("Produto cadastrado com sucesso!\n");
+}
+
+void listarProdutos() {
+
+    FILE *arquivo;
+    char linha[100];
+
+    arquivo = fopen("data/produtos.txt", "r");
+
+    if (arquivo == NULL) {
+        printf("Nenhum produto cadastrado!\n");
+        return;
+    }
+
+    printf("\n=== LISTA DE PRODUTOS ===\n");
+
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        printf("%s", linha);
+    }
+
+    fclose(arquivo);
+}
+
+void alterarProduto() {
+
+    FILE *arquivo;
+    FILE *temp;
+
+    int idBusca;
+    int id;
+    char nome[50];
+    float preco;
+    int encontrado = 0;
+
+    printf("Digite o ID do produto que deseja alterar: ");
+    scanf("%d", &idBusca);
+
+    arquivo = fopen("data/produtos.txt", "r");
+    temp = fopen("data/temp.txt", "w");
+
+    if (arquivo == NULL) {
+        printf("Arquivo nao encontrado!\n");
+        return;
+    }
+
+    while (fscanf(arquivo, "%d;%49[^;];%f\n",
+                  &id,
+                  nome,
+                  &preco) == 3) {
+
+        if (id == idBusca) {
+
+            encontrado = 1;
+
+            printf("Novo nome: ");
+            scanf(" %[^\n]", nome);
+
+            printf("Novo preco: ");
+            scanf("%f", &preco);
+        }
+
+        fprintf(temp,
+                "%d;%s;%.2f\n",
+                id,
+                nome,
+                preco);
+    }
+
+    fclose(arquivo);
+    fclose(temp);
+
+    remove("data/produtos.txt");
+    rename("data/temp.txt", "data/produtos.txt");
+
+    if (encontrado)
+        printf("Produto alterado com sucesso!\n");
+    else
+        printf("Produto nao encontrado!\n");
+}
+
+void excluirProduto() {
+
+    FILE *arquivo;
+    FILE *temp;
+
+    int idBusca;
+    int id;
+    char nome[50];
+    float preco;
+    int encontrado = 0;
+
+    printf("Digite o ID do produto que deseja excluir: ");
+    scanf("%d", &idBusca);
+
+    arquivo = fopen("data/produtos.txt", "r");
+    temp = fopen("data/temp.txt", "w");
+
+    if (arquivo == NULL) {
+        printf("Arquivo nao encontrado!\n");
+        return;
+    }
+
+    while (fscanf(arquivo, "%d;%49[^;];%f\n",
+                  &id,
+                  nome,
+                  &preco) == 3) {
+
+        if (id == idBusca) {
+            encontrado = 1;
+            continue;
+        }
+
+        fprintf(temp,
+                "%d;%s;%.2f\n",
+                id,
+                nome,
+                preco);
+    }
+
+    fclose(arquivo);
+    fclose(temp);
+
+    remove("data/produtos.txt");
+    rename("data/temp.txt", "data/produtos.txt");
+
+    if (encontrado)
+        printf("Produto excluido com sucesso!\n");
+    else
+        printf("Produto nao encontrado!\n");
+}
+
+
+
+
