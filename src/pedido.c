15b4b51597d7ca3615e7730374e9d024dd1cec45
Nyeidson Ryan <58894418+Tnbryan@users.noreply.github.com>
2026-06-17 20:15:37 +0000

    funcao pedido e correcoes

--- src/pedido.c
+++ src/pedido.c
@@ -8,7 +8,65 @@
 
 void criarPedido() {
     printf("Criar pedido\n");
+
+    FILE *arquivo;
+    int id;
+    char cliente[50];
+    int quantidade;
+    float preco;
+
+    arquivo = fopen("../data/pedidos.txt", "a");
+
+    if (arquivo == NULL) {
+        printf("Erro ao abrir arquivo!\n");
+        return;
+    }
+
+    printf("\n=== CADASTRAR PEDIDO ===\n");
+
+    printf("ID: ");
+    scanf("%d", &id);
+
+    printf("Nome do cliente : ");
+    scanf(" %[^\n]", cliente);
+
+    printf("Quantiade: ");
+    scanf("%d", &quantidade);
+
+    printf("Preco: ");
+    scanf("%f", &preco);
+
+    fprintf(arquivo, "%d;%s;%d,%.2f\n", id, cliente,quantidade, preco);
+
+    fclose(arquivo);
+
+    printf("Pedido cadastrado com sucesso!\n");
+    
+    printf("\nPressione ENTER para voltar...");
+    getchar();
+    getchar();
 }
 void listarPedidos() {
     printf("Listar pedidos\n");
+  FILE *arquivo;
+    char linha[100];
+
+    arquivo = fopen("../data/pedidos.txt", "r");
+
+    if (arquivo == NULL) {
+        printf("Nenhum produto cadastrado!\n");
+        return;
+    }
+
+    printf("\n=== LISTA DE PEDIDOS ===\n");
+
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        printf("%s", linha);
+    }
+
+    fclose(arquivo);
+
+    printf("\nPressione ENTER para voltar...");
+    getchar();
+    getchar();
 }
