542065dcaffc4e9e38d70456236bd15535123fa2
CarlosEduardo0702 <166566241+CarlosEduardo0702@users.noreply.github.com>
2026-06-18 17:47:39 -0300

    Update produto.c

--- src/produto.c
+++ src/produto.c
@@ -22,6 +22,7 @@
 
 #include <stdio.h>
 
+// Cadastrar Produto
 void cadastrarProduto() {
 
     FILE *arquivo;
@@ -42,9 +43,10 @@ void cadastrarProduto() {
 
     fclose(arquivo);
 
-    printf("Produto cadastrado!\n");
+    printf("Produto cadastrado com sucesso!\n");
 }
 
+// Listar Produtos
 void listarProdutos() {
 
     FILE *arquivo;
@@ -61,6 +63,76 @@ void listarProdutos() {
     fclose(arquivo);
 }
 
+// Alterar Produto
+void alterarProduto() {
 
+    FILE *arquivo;
+    FILE *temp;
 
+    int idBusca;
+    int id;
+    char nome[50];
+    char novoNome[50];
 
+    printf("\nDigite o ID do produto que deseja alterar: ");
+    scanf("%d", &idBusca);
+
+    arquivo = fopen("produtos.txt", "r");
+    temp = fopen("temp.txt", "w");
+
+    while (fscanf(arquivo, "%d;%[^\n]\n", &id, nome) == 2) {
+
+        if (id == idBusca) {
+
+            printf("Novo nome: ");
+            scanf(" %[^\n]", novoNome);
+
+            fprintf(temp, "%d;%s\n", id, novoNome);
+
+        } else {
+
+            fprintf(temp, "%d;%s\n", id, nome);
+        }
+    }
+
+    fclose(arquivo);
+    fclose(temp);
+
+    remove("produtos.txt");
+    rename("temp.txt", "produtos.txt");
+
+    printf("Produto alterado com sucesso!\n");
+}
+
+// Excluir Produto
+void excluirProduto() {
+
+    FILE *arquivo;
+    FILE *temp;
+
+    int idBusca;
+    int id;
+    char nome[50];
+
+    printf("\nDigite o ID do produto que deseja excluir: ");
+    scanf("%d", &idBusca);
+
+    arquivo = fopen("produtos.txt", "r");
+    temp = fopen("temp.txt", "w");
+
+    while (fscanf(arquivo, "%d;%[^\n]\n", &id, nome) == 2) {
+
+        if (id != idBusca) {
+
+            fprintf(temp, "%d;%s\n", id, nome);
+        }
+    }
+
+    fclose(arquivo);
+    fclose(temp);
+
+    remove("produtos.txt");
+    rename("temp.txt", "produtos.txt");
+
+    printf("Produto excluido com sucesso!\n");
+}
